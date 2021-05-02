1e448e14fe4d1372518046f97028cdb772259f00
DanielChu <jiahao.chu.dc@gmail.com>
2021-05-02 10:34:42 +0800

    create port function to read from env

--- start.go
+++ start.go
@@ -1,17 +1,28 @@
+
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func main() {
-	http.HandleFunc("/", index)       // index function will be called when route /
-	http.ListenAndServe(":8080", nil) //1
+func main(){
+	http.HandleFunc("/",index) // index function will be called when route //
+	http.ListenAndServe(port(),nil) 
+}
+
+func port() string {
+	// read from env
+	port := os.Getenv("PORT")
+	if len(port)==0 {
+		port = "8080"
+	}
+	return  ":" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello from cloud native go") // diff with printf
+   w.WriteHeader(http.StatusOK)
+	 fmt.Fprintf(w, "Hello from cloud native go") // diff with printf
 
 }
